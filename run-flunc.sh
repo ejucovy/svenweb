95aa86ff9ef5dd61f38d08be7336b66212590eec
egj <egj@40f73b84-6177-4a8e-8c61-7eee4b1974ed>
2010-10-28 19:00:40 +0000

    update ftests to run and pass against modern svenweb
    git-svn-id: https://svn.socialplanning.org/svn/svenweb/trunk@27680 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- run-flunc.sh
+++ run-flunc.sh
@@ -1,6 +1,4 @@
-rm -rf /tmp/svnrepo
-rm -rf /tmp/svnco
-svnadmin create /tmp/svnrepo
-svn co file:///tmp/svnrepo /tmp/svnco
-echo flunc -p ftests/ svenweb -D page_id=$1,page_id2=$2,rev_big=3,rev_small=2$4 $3
-flunc -p ftests/ svenweb -D page_id=$1,page_id2=$2,rev_big=3,rev_small=2$4 $3
+rm -rf /tmp/svenweb
+bzr init /tmp/svenweb
+echo flunc -t http://localhost:5052 -p ftests/ svenweb -D page_id=joe-page,page_id2=moe-page,rev_big=3,rev_small=2$2 $1
+flunc -t http://localhost:5052 -p ftests/ svenweb -D page_id=joe-page,page_id2=moe-page,rev_big=3,rev_small=2$2 $1
